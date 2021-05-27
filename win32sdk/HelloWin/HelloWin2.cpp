53d14c45e8c44fcd17e02b7b228465059515340b
ayushjaiswal-aj <ayushjaiswalaj@outlook.com>
2021-05-27 12:37:11 +0530

    creates simple GUI with grey backgound and hand as cursor

---
+++ win32sdk/HelloWin/HelloWin2.cpp
@@ -0,0 +1,107 @@
+#include <Windows.h>
+
+#pragma comment(lib, "kernel32.lib")
+#pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "user32.lib")
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpComLine, int nShowCmd){
+    static TCHAR szClassName[] = TEXT("The first Window");
+    static TCHAR szWindowCaption[] = TEXT("Window GUI - 3");
+
+    HBRUSH hBrush = NULL;
+    HCURSOR hCursor = NULL;
+    HICON hIcon = NULL;
+    HICON hIconSm = NULL;
+    HWND hWnd = NULL;
+
+    WNDCLASSEX wndEx;
+    MSG msg;
+
+    ZeroMemory (&wndEx, sizeof(WNDCLASSEX));
+    ZeroMemory (&msg, sizeof(MSG));
+
+    hBrush = (HBRUSH)GetStockObject(GRAY_BRUSH);
+    if(hBrush == NULL){
+        MessageBox((HWND)NULL, TEXT("Failed to aquire brush handle"), TEXT("GetStockObject"), MB_ICONERROR);
+        return (EXIT_FAILURE);
+    }
+
+    hCursor = LoadCursor((HINSTANCE)NULL, IDC_HAND);
+    if(hCursor == NULL){
+        MessageBox((HWND)NULL, TEXT("Failed to aquire cursor"), TEXT("LoadCursor"), MB_ICONERROR);
+        return (EXIT_FAILURE);
+    }
+
+    hIcon = LoadIcon((HINSTANCE)NULL, IDI_APPLICATION);
+    if(hIcon == NULL){
+        MessageBox((HWND)NULL, TEXT("Failed to aquire icon"), TEXT("LoadIcon"), MB_ICONERROR);
+        return (EXIT_FAILURE);
+    }
+
+    hIconSm = LoadIcon((HINSTANCE)NULL, IDI_APPLICATION);
+    if(hIconSm == NULL){
+        MessageBox((HWND)NULL, TEXT("Failed to aquire icon"), TEXT("LoadIcon"), MB_ICONERROR);
+        return (EXIT_SUCCESS);
+    }
+
+    wndEx.cbSize = sizeof(WNDCLASSEX);
+    wndEx.cbClsExtra = 0;
+    wndEx.cbWndExtra = 0;
+
+    wndEx.hbrBackground = hBrush;
+    wndEx.hCursor = hCursor;
+    wndEx.hIcon = hIcon;
+    wndEx.hIconSm = hIconSm;
+
+    wndEx.lpszClassName = szClassName;
+    wndEx.lpszMenuName = NULL;
+    wndEx.lpfnWndProc = WndProc;
+
+    wndEx.style = CS_HREDRAW | CS_VREDRAW;
+
+    if(!RegisterClassEx(&wndEx)){
+        MessageBox((HWND)NULL, TEXT("Failed to register window class"), TEXT("RegisterClassEx"), MB_ICONERROR);
+        return (EXIT_FAILURE);
+    }
+
+    hWnd = CreateWindowEx (
+        WS_EX_APPWINDOW,
+        szClassName,
+        szWindowCaption,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        (HWND)NULL,
+        (HMENU)NULL,
+        hInstance,
+        NULL
+    );
+
+    if(hWnd == NULL){
+        MessageBox((HWND)NULL, TEXT("Failed to aquire window handle"), TEXT("CreateClassEx"), MB_ICONERROR);
+        return (EXIT_FAILURE);
+    }
+
+    ShowWindow(hWnd, nShowCmd);
+    UpdateWindow(hWnd);
+
+    while (GetMessage(&msg, NULL, 0, 0)){
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (EXIT_SUCCESS);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+    switch (uMsg){
+        case WM_DESTROY:
+            PostQuitMessage(EXIT_SUCCESS);
+            break;
+    }
+    return (DefWindowProc(hWnd, uMsg, wParam, lParam));
+}
