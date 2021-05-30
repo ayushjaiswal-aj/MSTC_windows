8e17163f658f084bb0b52e9d5df73a83d1ecc7c0
ayushjaiswal-aj <ayushjaiswalaj@outlook.com>
2021-05-30 07:00:55 +0530

    creates simple window GUI

---
+++ win32sdk/HelloWin/HelloWin8.cpp
@@ -0,0 +1,105 @@
+#include <Windows.h>
+
+#pragma comment(lib, "kernel32.lib")
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
+    static TCHAR szClassName[] = TEXT("Gui window");
+    static TCHAR szCaptionWindow[] = TEXT("GUI - 9");
+
+    HBRUSH hBrush = NULL;
+    HCURSOR hCursor = NULL;
+    HICON hIcon = NULL;
+    HICON hIconSm = NULL;
+    HWND hWnd = NULL;
+
+    WNDCLASSEX wndEx;
+    MSG msg;
+
+    ZeroMemory(&wndEx, sizeof(WNDCLASSEX));
+    ZeroMemory(&msg, sizeof(MSG));
+
+    hBrush = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    if(hBrush == NULL){
+        MessageBox((HWND)NULL, TEXT("Failed to get handle to brush"), TEXT("GetStockObject"), MB_ICONERROR);
+        return (EXIT_FAILURE);
+    }
+
+    hCursor = LoadCursor((HINSTANCE)NULL, IDC_ARROW);
+    if(hCursor == NULL){
+        MessageBox((HWND)NULL, TEXT("Failed to get handle to cursor"), TEXT("LoadCursor"), MB_ICONERROR);
+        return (EXIT_FAILURE);
+    }
+
+    hIcon = LoadIcon((HINSTANCE)NULL, IDI_INFORMATION);
+    if(hIcon == NULL){
+        MessageBox((HWND)NULL, TEXT("Failed to get handle to icon"), TEXT("Load icon"), MB_ICONERROR);
+        return (EXIT_FAILURE);
+    }
+
+    hIconSm = LoadIcon((HINSTANCE)NULL, IDI_INFORMATION);
+    if(hIconSm == NULL){
+        MessageBox((HWND)NULL, TEXT("Failed to get handle to icon"), TEXT("LoadIcon"), MB_ICONERROR);
+        return (EXIT_FAILURE);
+    }
+
+    wndEx.hbrBackground = hBrush;
+    wndEx.hCursor = hCursor;
+    wndEx.hIcon = hIcon;
+    wndEx.hIconSm = hIconSm;
+    wndEx.cbSize = sizeof(WNDCLASSEX);
+    wndEx.cbClsExtra = 0;
+    wndEx.cbWndExtra = 0;
+    wndEx.lpszClassName = szClassName;
+    wndEx.lpszMenuName = NULL;
+    wndEx.lpfnWndProc = WndProc;
+    wndEx.style = CS_HREDRAW | CS_VREDRAW;
+
+    if(!RegisterClassEx(&wndEx)){
+        MessageBox((HWND)NULL, TEXT("Failed to register window class"), TEXT("RegisterClassEx"), MB_ICONERROR);
+        return (EXIT_FAILURE);
+    }
+
+    hWnd = CreateWindowEx(
+            WS_EX_APPWINDOW,
+            szClassName,
+            szCaptionWindow,
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            (HWND)NULL,
+            (HMENU)NULL,
+            hInstance,
+            NULL
+    );
+
+    if(hWnd == NULL){
+        MessageBox((HWND)NULL, TEXT("Failed to get handle to window"), TEXT("CreateWindowEx"), MB_ICONERROR);
+        return (EXIT_FAILURE);
+    }
+
+    ShowWindow(hWnd, nShowCmd);
+    UpdateWindow(hWnd);
+
+    while(GetMessage(&msg, (HWND)NULL, 0, 0)){
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (EXIT_SUCCESS);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+    switch(uMsg){
+        case WM_DESTROY:
+            PostQuitMessage(EXIT_SUCCESS);
+            break;
+    }
+
+    return (DefWindowProc(hWnd, uMsg, wParam, lParam));
+}
