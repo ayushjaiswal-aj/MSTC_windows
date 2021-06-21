0bc0ca8f9f045778ee08fdd88c3a32daa291af1c
ayushjaiswal-aj <ayushjaiswalaj@outlook.com>
2021-06-21 07:25:57 +0530

    creates window that shows contents of file

--- win32sdk/ReadFile/ShowFileWithVerticalScroll3.cpp
+++ win32sdk/ReadFile/ShowFileWithVerticalScroll3.cpp
@@ -1,18 +1,18 @@
-#include <stdio.h>
 #include <Windows.h>
+#include <stdio.h>
 #include "StringVector.h"
 
-#pragma comment(lib, "gdi32.lib")
-#pragma comment(lib, "user32.lib")
 #pragma comment(lib, "kernel32.lib")
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 PSTRINGVECTOR ReadFileInMemory(LPSTR lpFileName);
-void ReleaseAll(void);
+void ReleaseAll();
 
-#define FAIL_EXIT_IF(COND,MSG,CAPTION)      do\
+#define FAIL_EXIT_IF(COND,MSG,CAPTION)    do\
                                             {\
-                                                if((COND) == TRUE)\
+                                                if(COND == TRUE)\
                                                 {\
                                                     MessageBox((HWND)NULL, TEXT(MSG), TEXT(CAPTION), MB_ICONERROR);\
                                                     ReleaseAll();\
@@ -20,62 +20,60 @@ void ReleaseAll(void);
                                                 }\
                                             }while(0)
 
-LPSTR glpCmdLine = NULL;
-PSTRINGVECTOR gpStrVec = NULL;
+LPSTR glpCmdLine;
+PSTRINGVECTOR gpStrVec;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-    static TCHAR szClassName[] = TEXT("Standard Window");
+    static TCHAR szClassName[] = TEXT("Standerd Window");
     static TCHAR szWindowCaption[] = TEXT("Show File");
-
-    HBRUSH hBrush = NULL;
-    HCURSOR hCursor = NULL;
-    HICON hIcon = NULL;
-    HICON hIconSm = NULL;
-    HWND hWnd = NULL;
     ATOM bIsWindowClassRegistered = 0;
+    HWND hWnd = NULL;
 
     WNDCLASSEX wndEx;
+    ZeroMemory(&wndEx, sizeof(WNDCLASSEX));
     MSG msg;
+    ZeroMemory(&msg, sizeof(WNDCLASSEX));
 
     glpCmdLine = lpCmdLine;
 
-    ZeroMemory(&wndEx, sizeof(WNDCLASSEX));
-    ZeroMemory(&msg, sizeof(MSG));
+    HBRUSH hBrush = NULL;
+    hBrush = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    FAIL_EXIT_IF(hBrush == NULL, "Failed to get handle to brush", "GetStockObject");
 
-    hBrush = (HBRUSH)GetStockObject(WHITE_BRUSH);
-    FAIL_EXIT_IF(hBrush == NULL, "failed to get handle to brush", "GetStockObject");
-
-    hIcon = LoadIcon((HINSTANCE)NULL, IDI_APPLICATION);
-    FAIL_EXIT_IF(hIcon == NULL, "failed to get handle to icon", "LoadIcon");
-
-    hIconSm = LoadIcon((HINSTANCE)NULL, IDI_APPLICATION);
-    FAIL_EXIT_IF(hIconSm == NULL, "failed to get handle to icon", "LoadIcon");
-
+    HCURSOR hCursor = NULL;
     hCursor = LoadCursor((HINSTANCE)NULL, IDC_ARROW);
-    FAIL_EXIT_IF(hCursor == NULL, "failed to get handle to cursor", "LoadCursor");
+    FAIL_EXIT_IF(hCursor == NULL, "Failed to get handle to cursor", "LoadCurso");
 
-    wndEx.hbrBackground = hBrush;
-    wndEx.hIcon = hIcon;
-    wndEx.hIconSm = hIconSm;
-    wndEx.hCursor = hCursor;
+    HICON hIcon = NULL;
+    hIcon = LoadIcon((HINSTANCE)NULL, IDI_APPLICATION);
+    FAIL_EXIT_IF(hIcon == NULL, "Failed to get handle to icon", "LoadIcon");
+
+    HICON hIconSm = NULL;
+    hIconSm = LoadIcon((HINSTANCE)NULL, IDI_APPLICATION);
+    FAIL_EXIT_IF(hIconSm == NULL, "Failed to get handle to icon", "LoadIcon");
+
+    wndEx.cbSize = sizeof(WNDCLASSEX);
+    wndEx.cbClsExtra = 0;
+    wndEx.cbWndExtra = 0;
     wndEx.lpszClassName = szClassName;
     wndEx.lpszMenuName = NULL;
     wndEx.lpfnWndProc = WndProc;
-    wndEx.cbClsExtra = 0;
-    wndEx.cbWndExtra = 0;
-    wndEx.cbSize = sizeof(WNDCLASSEX);
+    wndEx.hbrBackground = hBrush;
+    wndEx.hCursor = hCursor;
+    wndEx.hIcon = hIcon;
+    wndEx.hIconSm = hIconSm;
     wndEx.hInstance = hInstance;
     wndEx.style = CS_HREDRAW | CS_VREDRAW;
-
+    
     bIsWindowClassRegistered = RegisterClassEx(&wndEx);
-    FAIL_EXIT_IF(bIsWindowClassRegistered == 0, "Failed to regiseter window class", "RegiseterClassEx");
+    FAIL_EXIT_IF(bIsWindowClassRegistered == 0, "Failed to register window class", "RegisterClassEx");
 
     hWnd = CreateWindowEx(
         WS_EX_APPWINDOW,
         szClassName,
         szWindowCaption,
-        WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_VSCROLL,
+        WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_VSCROLL | WS_THICKFRAME | WS_CAPTION | WS_SYSMENU,
         CW_USEDEFAULT,
         CW_USEDEFAULT,
         CW_USEDEFAULT,
@@ -86,12 +84,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         NULL
     );
 
-    FAIL_EXIT_IF(hWnd == NULL, "failed to create handle to window", "CreateWindowEx");
+    FAIL_EXIT_IF(hWnd == NULL, "Failed to create handle to window", "CreateWindowEx");
 
     ShowWindow(hWnd, nShowCmd);
     UpdateWindow(hWnd);
 
-    while(GetMessage(&msg, NULL, 0, 0))
+    while (GetMessage(&msg, NULL, 0, 0))
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
@@ -103,10 +101,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    static int cxChar, cyChar, cxCaps;
     static int cxClient, cyClient;
+    static int cxChar, cyChar, cxCaps;
     static int nVScrollPos = 0;
-    int nCnt = 0;
+    int nCnt;
 
     HDC hdc = NULL;
     PAINTSTRUCT ps;
@@ -114,34 +112,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
     switch (uMsg)
     {
+        case WM_SIZE:
+            cxClient = LOWORD(lParam);
+            cyClient = HIWORD(lParam);
+            break;
+
         case WM_CREATE:
             hdc = GetDC(hWnd);
             GetTextMetrics(hdc, &tm);
             cxChar = tm.tmAveCharWidth;
             cyChar = tm.tmHeight + tm.tmExternalLeading;
-            cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar /2;
+            cxCaps = (tm.tmPitchAndFamily & 1 ? 2 : 3) * cxChar/2;
             ReleaseDC(hWnd, hdc);
             hdc = NULL;
             gpStrVec = ReadFileInMemory(glpCmdLine);
             SetScrollRange(hWnd, SB_VERT, 0, gpStrVec->iVectorSize, FALSE);
             SetScrollPos(hWnd, SB_VERT, nVScrollPos, TRUE);
             break;
-
-        case WM_SIZE:
-            cxClient = LOWORD(lParam);
-            cyClient = HIWORD(lParam);
-            break;
-
-        case WM_PAINT:
-            hdc = BeginPaint(hWnd, &ps);
-            for(nCnt = 0; nCnt < gpStrVec->iVectorSize; nCnt ++)
-            {
-                TextOutA(hdc, 10, (nCnt - nVScrollPos) * cyChar, gpStrVec->ppString[nCnt], strlen(gpStrVec->ppString[nCnt]));
-            }
-            EndPaint(hWnd, &ps);
-            hdc = NULL;
-            break;
-
+        
         case WM_VSCROLL:
             switch(LOWORD(wParam))
             {
@@ -160,11 +148,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 case SB_PAGEDOWN:
                     nVScrollPos += (cyClient/cyChar);
                     break;
-
+                
                 case SB_THUMBPOSITION:
                     nVScrollPos = HIWORD(wParam);
                     break;
-                
+
                 default:
                     break;
             }
@@ -182,7 +170,18 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 InvalidateRect(hWnd, NULL, TRUE);
             }
             break;
+        
+        case WM_PAINT:
+            hdc = BeginPaint(hWnd, &ps);
+            for(nCnt = 0; nCnt < gpStrVec->iVectorSize; nCnt++)
+            {
+                TextOutA(hdc, 10, (nCnt - nVScrollPos) * cyChar, gpStrVec->ppString[nCnt], strlen(gpStrVec->ppString[nCnt]));
+            }
 
+            EndPaint(hWnd, &ps);
+            hdc = NULL;
+            break;
+        
         case WM_DESTROY:
             PostQuitMessage(EXIT_SUCCESS);
             break;
@@ -195,21 +194,22 @@ PSTRINGVECTOR ReadFileInMemory(LPSTR lpFileName)
 {
     #define BUFFER_SIZE 128
 
+    int nStrLen;
     FILE *fp = NULL;
     CHAR chBuffer[BUFFER_SIZE];
+    memset(chBuffer, 0, BUFFER_SIZE);
+
     PCHAR pStr = NULL;
-    int nStrLen;
 
     fp = fopen(lpFileName, "r");
-    FAIL_EXIT_IF(fp == NULL, "could not open input file", "fopen");
+    FAIL_EXIT_IF(fp == NULL, "Failed to open file", "fopen");
 
     gpStrVec = CreateStringVector();
-    memset(chBuffer, 0, sizeof(BUFFER_SIZE));
 
     while(fgets(chBuffer, BUFFER_SIZE, fp) != NULL)
     {
         nStrLen = strnlen_s(chBuffer, BUFFER_SIZE);
-        pStr = (PCHAR)Xcalloc(1, nStrLen + 1);
+        pStr = (PCHAR) Xcalloc(1, nStrLen + 1);
         strncpy(pStr, chBuffer, nStrLen);
         PushBack(gpStrVec, pStr);
         memset(chBuffer, 0, BUFFER_SIZE);
@@ -217,8 +217,7 @@ PSTRINGVECTOR ReadFileInMemory(LPSTR lpFileName)
 
     fclose(fp);
     fp = NULL;
-
-    return (gpStrVec);
+    return gpStrVec;
 }
 
 void ReleaseAll(void)
--- win32sdk/ReadFile/ShowFileWithVerticalScroll4.cpp
+++ win32sdk/ReadFile/ShowFileWithVerticalScroll4.cpp
@@ -1,81 +1,79 @@
-#include <stdio.h>
 #include <Windows.h>
+#include <stdio.h>
 #include "StringVector.h"
 
-#pragma comment(lib, "gdi32.lib")
-#pragma comment(lib, "user32.lib")
 #pragma comment(lib, "kernel32.lib")
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-PSTRINGVECTOR ReadFileInMemory(LPSTR lpFileName);
 void ReleaseAll(void);
-
-#define FAIL_EXIT_IF(COND,MSG,CAPTION)      do\
+PSTRINGVECTOR ReadFileInMemory(LPSTR lpFileLine);
+/*
+#define FAIL_EXIT_IF(COND,MSG,CAPTION)  do\
+                                        {\
+                                            if(COND == TRUE)\
                                             {\
-                                                if((COND) == TRUE)\
-                                                {\
-                                                    MessageBox((HWND)NULL, TEXT(MSG), TEXT(CAPTION), MB_ICONERROR);\
-                                                    ReleaseAll();\
-                                                    ExitProcess(EXIT_FAILURE);\
-                                                }\
-                                            }while(0)
-
-LPSTR glpCmdLine = NULL;
+                                                MessageBox((HWND)NULL, TEXT(MSG), TEXT(CAPTION), MB_ICONERROR);\
+                                                ReleaseAll();\
+                                                ExitProcess(EXIT_FAILURE);\
+                                            }\
+                                        }while(0)
+*/
 PSTRINGVECTOR gpStrVec = NULL;
+LPSTR glpCmdLine = NULL;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-    static TCHAR szClassName[] = TEXT("Standard Window");
+    static TCHAR szClassName[] = TEXT("Standard window");
     static TCHAR szWindowCaption[] = TEXT("Show File");
-
-    HBRUSH hBrush = NULL;
-    HCURSOR hCursor = NULL;
-    HICON hIcon = NULL;
-    HICON hIconSm = NULL;
     HWND hWnd = NULL;
     ATOM bIsWindowClassRegistered = 0;
 
-    WNDCLASSEX wndEx;
-    MSG msg;
-
     glpCmdLine = lpCmdLine;
 
+    WNDCLASSEX wndEx;
     ZeroMemory(&wndEx, sizeof(WNDCLASSEX));
+    MSG msg;
     ZeroMemory(&msg, sizeof(MSG));
 
+    HBRUSH hBrush = NULL;
     hBrush = (HBRUSH)GetStockObject(WHITE_BRUSH);
-    FAIL_EXIT_IF(hBrush == NULL, "failed to get handle to brush", "GetStockObject");
+    FAIL_EXIT_IF(hBrush == NULL, "Failed to get handle to brush", "GetStockObject");
 
+    HCURSOR hCursor = NULL;
+    hCursor = LoadCursor((HINSTANCE)NULL, IDC_ARROW);
+    FAIL_EXIT_IF(hCursor == NULL, "Failed to get handle to cursor", "LoadCursor");
+
+    HICON hIcon = NULL;
     hIcon = LoadIcon((HINSTANCE)NULL, IDI_APPLICATION);
-    FAIL_EXIT_IF(hIcon == NULL, "failed to get handle to icon", "LoadIcon");
+    FAIL_EXIT_IF(hIcon == NULL, "Failed to get handle to icon", "LoadIcon");
 
+    HICON hIconSm = NULL;
     hIconSm = LoadIcon((HINSTANCE)NULL, IDI_APPLICATION);
     FAIL_EXIT_IF(hIconSm == NULL, "failed to get handle to icon", "LoadIcon");
 
-    hCursor = LoadCursor((HINSTANCE)NULL, IDC_ARROW);
-    FAIL_EXIT_IF(hCursor == NULL, "failed to get handle to cursor", "LoadCursor");
-
-    wndEx.hbrBackground = hBrush;
-    wndEx.hIcon = hIcon;
-    wndEx.hIconSm = hIconSm;
-    wndEx.hCursor = hCursor;
+    wndEx.cbSize = sizeof(WNDCLASSEX);
+    wndEx.cbClsExtra = 0;
+    wndEx.cbWndExtra = 0;
     wndEx.lpszClassName = szClassName;
     wndEx.lpszMenuName = NULL;
     wndEx.lpfnWndProc = WndProc;
-    wndEx.cbClsExtra = 0;
-    wndEx.cbWndExtra = 0;
-    wndEx.cbSize = sizeof(WNDCLASSEX);
+    wndEx.hbrBackground = hBrush;
+    wndEx.hCursor = hCursor;
+    wndEx.hIcon = hIcon;
+    wndEx.hIconSm = hIconSm;
     wndEx.hInstance = hInstance;
     wndEx.style = CS_HREDRAW | CS_VREDRAW;
 
     bIsWindowClassRegistered = RegisterClassEx(&wndEx);
-    FAIL_EXIT_IF(bIsWindowClassRegistered == 0, "Failed to regiseter window class", "RegiseterClassEx");
+    FAIL_EXIT_IF(bIsWindowClassRegistered == 0, "Failed to  register window class", "RegisterClassEx");
 
     hWnd = CreateWindowEx(
         WS_EX_APPWINDOW,
         szClassName,
         szWindowCaption,
-        WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_VSCROLL,
+        WS_THICKFRAME | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_CAPTION | WS_VSCROLL,
         CW_USEDEFAULT,
         CW_USEDEFAULT,
         CW_USEDEFAULT,
@@ -86,8 +84,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         NULL
     );
 
-    FAIL_EXIT_IF(hWnd == NULL, "failed to create handle to window", "CreateWindowEx");
-
+    FAIL_EXIT_IF(hWnd == NULL, "Failed to create handle to window", "CreateWindowEx");
+    
     ShowWindow(hWnd, nShowCmd);
     UpdateWindow(hWnd);
 
@@ -103,49 +101,39 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    static int cxChar, cyChar, cxCaps;
     static int cxClient, cyClient;
+    static int cxChar, cyChar, cxCaps;
     static int nVScrollPos = 0;
-    int nCnt = 0;
+    int nCnt;
 
     HDC hdc = NULL;
     PAINTSTRUCT ps;
     TEXTMETRIC tm;
 
-    switch (uMsg)
+    switch(uMsg)
     {
-        case WM_CREATE:
+        case WM_SIZE:
+            cxClient = LOWORD(lParam);
+            cyClient = HIWORD(lParam);
+            break;
+        
+        case WM_CREATE: 
             hdc = GetDC(hWnd);
             GetTextMetrics(hdc, &tm);
             cxChar = tm.tmAveCharWidth;
             cyChar = tm.tmHeight + tm.tmExternalLeading;
-            cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar /2;
+            cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar / 2;
             ReleaseDC(hWnd, hdc);
             hdc = NULL;
             gpStrVec = ReadFileInMemory(glpCmdLine);
             SetScrollRange(hWnd, SB_VERT, 0, gpStrVec->iVectorSize, FALSE);
             SetScrollPos(hWnd, SB_VERT, nVScrollPos, TRUE);
             break;
-
-        case WM_SIZE:
-            cxClient = LOWORD(lParam);
-            cyClient = HIWORD(lParam);
-            break;
-
-        case WM_PAINT:
-            hdc = BeginPaint(hWnd, &ps);
-            for(nCnt = 0; nCnt < gpStrVec->iVectorSize; nCnt ++)
-            {
-                TextOutA(hdc, 10, (nCnt - nVScrollPos) * cyChar, gpStrVec->ppString[nCnt], strlen(gpStrVec->ppString[nCnt]));
-            }
-            EndPaint(hWnd, &ps);
-            hdc = NULL;
-            break;
-
+        
         case WM_VSCROLL:
             switch(LOWORD(wParam))
             {
-                case SB_LINEUP:
+                case SB_LINEUP: 
                     nVScrollPos -= 1;
                     break;
                 
@@ -160,7 +148,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 case SB_PAGEDOWN:
                     nVScrollPos += (cyClient/cyChar);
                     break;
-
+                
                 case SB_THUMBPOSITION:
                     nVScrollPos = HIWORD(wParam);
                     break;
@@ -174,7 +162,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             }
             else if(nVScrollPos > gpStrVec->iVectorSize - 1)
             {
-                nVScrollPos = gpStrVec->iVectorSize - 1;
+                nVScrollPos = gpStrVec->iVectorSize-1;
             }
             else 
             {
@@ -182,12 +170,21 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 InvalidateRect(hWnd, NULL, TRUE);
             }
             break;
+        
+        case WM_PAINT:
+            hdc = BeginPaint(hWnd, &ps);
+            for(nCnt = 0; nCnt < gpStrVec->iVectorSize; nCnt++)
+            {
+                TextOutA(hdc, 10, (nCnt - nVScrollPos)*cyChar, gpStrVec->ppString[nCnt], strlen(gpStrVec->ppString[nCnt]));
+            }
+            EndPaint(hWnd, &ps);
+            hdc = NULL;
+            break;
 
         case WM_DESTROY:
             PostQuitMessage(EXIT_SUCCESS);
             break;
     }
-
     return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
@@ -195,26 +192,25 @@ PSTRINGVECTOR ReadFileInMemory(LPSTR lpFileName)
 {
     #define BUFFER_SIZE 128
 
+    int strlen;
     FILE *fp = NULL;
     CHAR chBuffer[BUFFER_SIZE];
+    memset(chBuffer, 0, BUFFER_SIZE);
     PCHAR pStr = NULL;
-    int nStrLen;
 
     fp = fopen(lpFileName, "r");
-    FAIL_EXIT_IF(fp == NULL, "could not open input file", "fopen");
+    FAIL_EXIT_IF(fp == NULL, "failed to open file", "fopen");
 
     gpStrVec = CreateStringVector();
-    memset(chBuffer, 0, sizeof(BUFFER_SIZE));
 
     while(fgets(chBuffer, BUFFER_SIZE, fp) != NULL)
     {
-        nStrLen = strnlen_s(chBuffer, BUFFER_SIZE);
-        pStr = (PCHAR)Xcalloc(1, nStrLen + 1);
-        strncpy(pStr, chBuffer, nStrLen);
+        strlen = strnlen_s(chBuffer, BUFFER_SIZE);
+        pStr = (PCHAR)Xcalloc(1, strlen + 1);
+        strncpy(pStr, chBuffer, strlen);
         PushBack(gpStrVec, pStr);
         memset(chBuffer, 0, BUFFER_SIZE);
     }
-
     fclose(fp);
     fp = NULL;
 
@@ -229,3 +225,4 @@ void ReleaseAll(void)
         gpStrVec = NULL;
     }
 }
+
