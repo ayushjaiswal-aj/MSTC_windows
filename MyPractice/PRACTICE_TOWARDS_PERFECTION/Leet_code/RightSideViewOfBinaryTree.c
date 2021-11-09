3d2b89f4dd1f0ac6159f68b7099f5f23124331fc
Ayush Jaiswal <ayushjaiswalaj@outlook.com>
2021-11-09 17:33:25 +0530

    Right side view of binary tree

---
+++ MyPractice/PRACTICE_TOWARDS_PERFECTION/Leet_code/RightSideViewOfBinaryTree.c
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int maxLevel = 0;
+int count = 0;
+
+void rightViewHelper(struct TreeNode *root, int *retArray, int level);
+int* rightSideView(struct TreeNode *root, int *returnSize);
+
+int main(void){
+    
+}
+
+void rightViewHelper(struct TreeNode *root, int *retArray, int level){
+    if(root == NULL){
+        return;
+    }
+    if(maxLevel < level){
+        retArray[count] = root->val;
+        count++;
+        maxLevel = level;
+    }
+    
+    rightViewHelper(root->right, retArray, level+1);
+    rightViewHelper(root->left, retArray, level+1);
+}
+
+int* rightSideView(struct TreeNode* root, int* returnSize){
+    int *retArray = NULL;
+    
+    retArray = (int *)malloc(sizeof(int) * 100);
+    rightViewHelper(root, retArray, 1);
+    *returnSize = count;
+    return retArray;
+}
